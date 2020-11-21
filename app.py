67ad9e778e430549d878e6c463730a340d5f7ff2
Shaurya Sanghvi <shaurya@s_berkeley.edu>
2020-11-21 01:56:06 -0500

    instantiated form for clickbait

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import pickle
 
 # load model
@@ -10,35 +10,34 @@ vectorizer = pickle.load(open('vectorizer.pkl','rb'))
 app = Flask(__name__)
 
 # routes
-@app.route('/', methods=['POST'])
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/predict',methods=['POST'])
 def predict():
     # get data
-    data = request.get_json(force=True)
-    
-    # data.update((x, [y]) for x, y in data.items())
-    # data_df = pd.DataFrame.from_dict(data)
-
-    # #converts df to list
-    # data_df.values.tolist()
-
+    data = [str(x) for x in request.form.values()]
     new_vectors = vectorizer.transform(data)
     new_predictions = svm.predict(new_vectors)
     
-    # # convert data into dataframe
-    # data.update((x, [y]) for x, y in data.items())
-    # data_df = pd.DataFrame.from_dict(data)
-
-    # # predictions
-    # result = model.predict(data_df)
-
-    # send back to browser
     print(new_predictions)
     print(type(new_predictions))
     output = {'new_predictions': new_predictions.tolist()}
+    for i,row in enumerate(output):
+     output[i] = {k:("Clickbait" if 1 else "Not Clickbait") for k,v in row.items()}
 
     # return data
-    return jsonify(results=output)
+    # return jsonify(results=output)
+
+    # data = [str(x) for x in request.form.values()]
+    # final_features = [np.array(int_features)]
+    # prediction = model.predict(final_features)
+
+    # output = round(prediction[0], 2)
+
+
+    return render_template('index.html', prediction_text='Results Are:  $ {}'.format(output))
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
