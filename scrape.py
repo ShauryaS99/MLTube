136190b1746e5c8af25a8cf44e8381b71aba40f5
Shaurya Sanghvi <shaurya@s_berkeley.edu>
2020-12-04 23:50:19 -0500

    oh lord plz help

--- scrape.py
+++ scrape.py
@@ -3,6 +3,9 @@ from rake_nltk import Rake
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
 
